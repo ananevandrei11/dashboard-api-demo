cf67dd7a79bb3cdc5c80346816119faec0071f91
ananevandrei11 <ananevandrei11@gmail.com>
2026-06-08 15:14:51 +0200

    set express

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-import http from 'http';
+import http from "http";
+import express from "express";
 
 const port = 8000;
-const host = '127.0.0.1';
-const server = http.createServer((req, res) => {
-  if (req.url === "/ping") {
-    res.setHeader('Content-Type', 'text/plain');
-    res.statusCode = 200;
-    res.end('Pong!')
-    return;
-  }
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    data: 'Hello World!',
-  }));
+const app = express();
+
+app.get("/ping", (req, res) => {
+  res.send("Pong!");
 });
 
-server.listen(port, host, () => console.log(`SERVER STARTED on host:${host} port:${port}`));
+app.listen(port, () => {
+  console.log(`Example app listening on host: http://localhost:${port}`);
+});
